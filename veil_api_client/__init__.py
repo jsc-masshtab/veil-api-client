1819247ec4301cd467e0bec030612398e31aac3b
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2020-07-06 13:53:00 +0300

    FEATURE TG-8886 alternate singleton
    альтернативная реализация синглтона для упрощения работы из VDI.

--- veil_api_client/__init__.py
+++ veil_api_client/__init__.py
@@ -9,10 +9,10 @@ __version__ = '2.0.0'
 
 from .api_objects.domain import DomainConfiguration
 from .base.api_object import VeilRestPaginator
-from .https_client import VeilClient
+from .https_client import VeilClient, VeilClientSingleton
 
 __all__ = (
-    'VeilClient', 'VeilRestPaginator', 'DomainConfiguration'
+    'VeilClient', 'VeilRestPaginator', 'DomainConfiguration', 'VeilClientSingleton'
 )
 
 __author__ = 'Aleksey Devyatkin <a.devyatkin@mashtab.org>'
