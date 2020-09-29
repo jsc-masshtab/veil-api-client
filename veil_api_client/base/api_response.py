a34efa7ca011fb3000965ad6479d58f10961a05d
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2020-09-29 16:06:13 +0300

    FEATURE TG-9952: VeilApiResponse updates.
    property error_code и error_detail

--- veil_api_client/base/api_response.py
+++ veil_api_client/base/api_response.py
@@ -16,6 +16,13 @@ class VeilApiResponse:
 
     Properties:
         paginator_results: value of results key from response data. May presents only in list() queries.
+        value: value of single-count entity from response data. May present in info() query.
+        response: list with calling VeilApiObject entities instances.
+        task: VeilTask if response status_code is 202.
+        success: success flag of response.
+        error_code: VeiL error code from response. If there are several errors - only 1st will be returned.
+        error_detail: VeiL error detail from response. If there are several errors - only 1st will be returned.
+
     """
 
     __SUCCESS_STATUSES = frozenset((200, 201, 202, 204))
@@ -84,3 +91,48 @@ class VeilApiResponse:
     def success(self) -> bool:
         """Determine that requests response goes well."""
         return self.status_code in self.__SUCCESS_STATUSES
+
+    @property
+    def error_code(self) -> int:
+        """Return VeiL error code from response.
+
+        Note:
+             If response is successful error_code will be 0.
+             If error_code can`t be determined value will be 50000.
+
+        Example of VeiL error response: {'errors': [{'code': '50004', 'detail': 'URL is not found.'}]}
+        """
+        if self.success:
+            return 0
+        # extract error code from response
+        try:
+            error_list = self.data.get('errors')
+            error_code_str = error_list[0]['code']
+        except (AttributeError, KeyError, TypeError, IndexError):
+            error_code_str = '50000'
+        # convert error code to int
+        try:
+            error_code = int(error_code_str)
+        except ValueError:
+            error_code = 50000
+        return error_code
+
+    @property
+    def error_detail(self) -> str:
+        """Return Veil error detail from response.
+
+        Note:
+             If response is successful error_code will be None.
+             If error_detail can`t be determined value will be None.
+
+        Example of VeiL error response: {'errors': [{'code': '50004', 'detail': 'URL is not found.'}]}
+        """
+        if self.success:
+            return
+        # extract error detail from response
+        try:
+            error_list = self.data.get('errors')
+            error_detail = error_list[0]['detail']
+        except (AttributeError, KeyError, TypeError, IndexError):
+            error_detail = None
+        return error_detail
