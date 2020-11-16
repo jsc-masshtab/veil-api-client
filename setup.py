3bd59837d89e1bed968cfe908bc6dbf1b42d9cbb
Aleksey Devyatkin <a.devyatkin@mashtab.org>
2020-11-16 20:00:11 +0300

    FEATURE TG-10732: Retry for every request.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='veil-api-client',
-    version='2.0.2',
+    version='2.0.3',
     author='Aleksey Devyatkin',
     author_email='a.devyatkin@mashtab.org',
     description='VeiL ECP Api client',
